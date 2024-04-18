1b319a7257db321c3126727a61da8a89fd2c3a1b
balesniy <balesniy@yandex.ru>
2024-04-18 11:13:16 +0200

    Update hello.mjs

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,4 +1,7 @@
 export default async (req, context) => {
+  console.log('event', req)
+    console.log('handler', req.queryStringParameters)
+
   return new Response(`Hello, ${req.queryStringParameters?.query || Date.now()}`, {
     headers: {
        'access-control-allow-origin': '*'
